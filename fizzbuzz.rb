2779ba3904ac2d7396ed587c9ac3dab13738b746
Andrew Bourgeois <andrew@olympicmedia.com>
2020-03-04 17:09:03 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,11 @@
 
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
-    "Fizz"
-  elsif int % 3 == 0
-    "Buzz"
-  elsif int % 5 == 0
     "FizzBuzz"
+  elsif int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Fizz"
   else
     return nil
   end
